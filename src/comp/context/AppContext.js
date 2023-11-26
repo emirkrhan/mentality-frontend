7d34dca9c017701e4e0094e884b5f1a57ffc77e6
Emirhan Körhan <korhanemirhann@gmail.com>
2023-11-26 15:48:11 +0300

    end-edit-two

--- src/comp/context/AppContext.js
+++ src/comp/context/AppContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import BotService from "../../service/BotService";
 
 const AppContext = createContext();
